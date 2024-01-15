4b8fa203d4df120b07a01b5c1c920e8c4e2a71ea
LeeYongSung <dldydtjd9@naver.com>
2024-01-15 23:03:56 +0900

    clean project

---
+++ campon_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:campon_app/example/IntroScreen/onbording.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'example/Utils/dark_lightmode.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ColorNotifire()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CampOn',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const onbording(),
+    );
+  }
+}
